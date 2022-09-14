c2ceb7ee133c27f17cd6c356d51ba22b8331590d
Mateusz Koszutowski <mkoszut@gmail.com>
2022-09-14 08:59:12 +0200

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-X_USER_ID=$1
-X_API_KEY=$2
-NAME=$3
-TAG=$4
-IMAGE=$5
-PATH=$6
-PORT=$7
-HAS_BASE_PATH=$8
+# X_USER_ID=$1
+# X_API_KEY=$2
+# NAME=$3
+# TAG=$4
+# IMAGE=$5
+# PATH=$6
+# PORT=$7
+# HAS_BASE_PATH=$8
 
-echo $X_USER_ID
+# echo $X_USER_ID
 
-ls -al ~/
+# ls -al ~/
 
-whereis env
+# whereis env
 
-env
+# env
 
 
 time=$(date)
