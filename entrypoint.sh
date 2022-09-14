043211f750b02c64f6ce7b325751a40bc71c5b6e
Mateusz Koszutowski <mkoszut@gmail.com>
2022-09-14 09:36:55 +0200

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ DOC_PATH=$6
 DOC_PORT=$7
 DOC_HAS_BASE_PATH=$8
 
+DOC_INSTANCE_NAMESPACE=
+
 echo $X_USER_ID
 
 ls -al ~/
@@ -18,6 +20,22 @@ ls -al ~/
 env
 
 curl -I -X GET https://google.com/
+
+curl -H X-User-Id: "$X_USER_ID'  -H "X-Api-Key: $X_API_KEY" -H "Content-Type: application/json" -X GET https://farmer.storefrontcloud.io/instance/docs-europe-west1-gcp-storefrontcloud-io
+
+# curl -H X-User-Id: "$X_USER_ID'  -H "X-Api-Key: $X_API_KEY" -H 'Content-Type: application/json' -X PATCH -d '
+  # {
+  #   "additional_apps":{
+  #     "apps":[{
+  #       "name":"docs-v2-bigcommerce",
+  #       "tag":"${{ steps.get_version.outputs.VERSION }}",
+  #       "image":"registry.storefrontcloud.io/docs-storefrontcloud-io/v2-bigcommerce",
+  #       "path":"/bigcommerce",
+  #       "port":"80"
+  #     }]
+  #   }
+  # }' https://farmer.storefrontcloud.io/instance/docs-europe-west1-gcp-storefrontcloud-io
+
 
 
 time=$(date)
